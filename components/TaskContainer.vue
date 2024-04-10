ba9f086e5710d7ba7083d31c8b8e042f97e6b865
cavadosi <jpeque.moab@gmail.com>
2024-04-10 11:27:59 +0200

    fix TaskContainer scrollbar position

--- components/TaskContainer.vue
+++ components/TaskContainer.vue
@@ -1,23 +1,25 @@
 <template>
-  <div
-    class="flex-col items-center justify-center min-w-fit text-start bg-slate-100 rounded-md px-6 py-4 h-screen overflow-y-auto scrollbar"
-  >
-    <!-- Container title -->
-    <div class="flex items-center justify-between mb-4">
-      <h1>{{ columnType }}</h1>
-      <h1>...</h1>
-    </div>
+  <div class="bg-slate-100 rounded-md px-1">
     <div
-      class="flex-col items-center justify-center space-y-4 min-w-80 w-80 text-start bg-slate-100"
+      class="flex-col items-center justify-center min-w-fit text-start px-5 py-4 h-screen overflow-y-auto scrollbar"
     >
-      <!-- Card -->
-      <TaskCard
-        v-for="task in tasks"
-        :key="task.title"
-        :title="task.title"
-        :type="task.type"
-        :columnType="columnType"
-      />
+      <!-- Container title -->
+      <div class="flex items-center justify-between mb-4">
+        <h1>{{ columnType }}</h1>
+        <h1>...</h1>
+      </div>
+      <div
+        class="flex-col items-center justify-center space-y-4 min-w-80 w-80 text-start bg-slate-100"
+      >
+        <!-- Card -->
+        <TaskCard
+          v-for="task in tasks"
+          :key="task.title"
+          :title="task.title"
+          :type="task.type"
+          :columnType="columnType"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -26,7 +28,7 @@
 const props = defineProps({
   columnType: {
     type: String,
-    default: '', // Default value for columnType (if not provided)
+    default: "", // Default value for columnType (if not provided)
   },
   tasks: {
     type: Array,
@@ -40,4 +42,3 @@ const { columnType, tasks } = toRefs(props);
 console.log(columnType.value); // Check if columnType is defined and has a value
 console.log(tasks.value); // Check if tasks array is defined and contains data
 </script>
- 
