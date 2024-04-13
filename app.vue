40e2a50b28f1734d941795317b2a46b39ca8d0a3
cavadosi <jpeque.moab@gmail.com>
2024-04-13 12:17:00 +0200

    Commit and push linter with husky, eslint and prettier

--- app.vue
+++ app.vue
@@ -1,33 +1,33 @@
 <template>
-  <Html class="h-full">
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </Html>
+	<Html class="h-full">
+		<NuxtLayout>
+			<NuxtPage />
+		</NuxtLayout>
+	</Html>
 </template>
 
 <style>
 html {
-  height: 100%;
-  overflow: clip;
+	height: 100%;
+	overflow: clip;
 }
 
 body {
-  background-color: #D8EFFF;
-  min-height: 100%;
-  margin: 0;
-  position: relative;
-  z-index: 1;
+	background-color: #ffffff;
+	min-height: 100%;
+	margin: 0;
+	position: relative;
+	z-index: 1;
 }
 
 body::before {
-  content: "";
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: -1;
-  background-size: cover;
+	content: '';
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	z-index: -1;
+	background-size: cover;
 }
 </style>
