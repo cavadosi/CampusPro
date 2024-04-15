eda98c0f71523a29020fea19d71a69396205a67d
cavadosi <jpeque.moab@gmail.com>
2024-04-15 20:35:49 +0200

    FAQ section

--- pages/landing/index.vue
+++ pages/landing/index.vue
@@ -83,7 +83,31 @@
 						</div>
 					</section>
 					<!-- FAQ section -->
-					<section id="FAQ" class="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4"></section>
+					<section id="FAQ" class="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+						<div class="bg-white">
+							<div class="mx-auto max-w-7xl px-6 py-12 sm:py-16 lg:px-8 lg:py-20">
+								<div class="mx-auto max-w-4xl divide-y divide-gray-900/10">
+									<h2 class="text-2xl font-bold leading-10 tracking-tight text-gray-900">Frequently asked questions</h2>
+									<dl class="mt-10 space-y-6 divide-y divide-gray-900/10">
+										<Disclosure as="div" v-for="faq in faqs" :key="faq.question" class="pt-6" v-slot="{ open }">
+											<dt>
+												<DisclosureButton class="flex w-full items-start justify-between text-left text-gray-900">
+													<span class="text-base font-semibold leading-7">{{ faq.question }}</span>
+													<span class="ml-6 flex h-7 items-center">
+														<IconHeroicons:plus-20-solid v-if="!open" class="h-6 w-6" aria-hidden="true" />
+														<IconHeroicons:minus-20-solid v-else class="h-6 w-6" aria-hidden="true" />
+													</span>
+												</DisclosureButton>
+											</dt>
+											<DisclosurePanel as="dd" class="mt-2 pr-12">
+												<p class="text-base leading-7 text-gray-600">{{ faq.answer }}</p>
+											</DisclosurePanel>
+										</Disclosure>
+									</dl>
+								</div>
+							</div>
+						</div>
+					</section>
 					<!-- CTA section -->
 					<section id="CTA" class="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
 						<div class="bg-white">
@@ -112,11 +136,31 @@
 </template>
 
 <script setup>
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue';
+
 const stats = [
 	{ label: 'Founded', value: '2021' },
 	{ label: 'Employees', value: '37' },
 	{ label: 'Countries', value: '12' },
 	{ label: 'Raised', value: '$25M' }
+];
+const faqs = [
+	{
+		question: "What's the best thing about Switzerland?",
+		answer: "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat."
+	},
+	{
+		question: "What's the best thing about Switzerland?",
+		answer: "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat."
+	},
+	{
+		question: "What's the best thing about Switzerland?",
+		answer: "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat."
+	},
+	{
+		question: "What's the best thing about Switzerland?",
+		answer: "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat."
+	}
 ];
 definePageMeta({
 	layout: 'landing'
