9ce6e7136ab4f7efe43667bbbc6d9c87cd47b87d
cavadosi <jpeque.moab@gmail.com>
2024-04-15 21:08:48 +0200

    Landing index layout done

--- pages/landing/index.vue
+++ pages/landing/index.vue
@@ -25,37 +25,116 @@
 				</div>
 				<div class="mx-auto space-y-16 max-w-7xl px-6 lg:px-8 mt-20">
 					<!-- Key features section -->
-					<section id="KeyFeatures" class="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-						<div class="bg-white py-24 sm:py-32">
-							<div class="mx-auto max-w-7xl px-6 lg:px-8">
-								<div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-									<div>
-										<h2 class="text-base font-semibold leading-7 text-primary">Everything you need</h2>
-										<p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">All-in-one platform</p>
-										<p class="mt-6 text-base leading-7 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
-									</div>
-									<dl class="col-span-2 grid grid-cols-1 gap-x-8 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:gap-y-16">
-										<div v-for="feature in features" :key="feature.name" class="relative pl-9">
-											<dt class="font-semibold text-gray-900">
-												<IconHeroicons:check class="absolute left-0 top-1 h-5 w-5 text-primary-300" aria-hidden="true" />
-												{{ feature.name }}
-											</dt>
-											<dd class="mt-2">{{ feature.description }}</dd>
-										</div>
-									</dl>
+					<section id="KeyFeatures" class="py-24 sm:py-32">
+						<div class="mx-auto max-w-7xl px-6 lg:px-8">
+							<div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+								<div>
+									<h2 class="text-base font-semibold leading-7 text-primary">Everything you need</h2>
+									<p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">All-in-one platform</p>
+									<p class="mt-6 text-base leading-7 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
 								</div>
+								<dl class="col-span-2 grid grid-cols-1 gap-x-8 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:gap-y-16">
+									<div v-for="feature in features" :key="feature.name" class="relative pl-9">
+										<dt class="font-semibold text-gray-900">
+											<IconHeroicons:check class="absolute left-0 top-1 h-5 w-5 text-primary-300" aria-hidden="true" />
+											{{ feature.name }}
+										</dt>
+										<dd class="mt-2">{{ feature.description }}</dd>
+									</div>
+								</dl>
 							</div>
 						</div>
 					</section>
 
 					<!-- How it works section -->
-					<section id="HowItWorks" class="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4"></section>
-					<!-- Benefits for students section -->
-					<section id="Benefits" class="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4"></section>
+					<section id="HowItWorks" class="overflow-hidden bg-white py-24 sm:py-32">
+						<div class="mx-auto max-w-7xl md:px-6 lg:px-8">
+							<div class="grid grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:grid-cols-2 lg:items-start">
+								<div class="px-6 lg:px-0 lg:pr-4 lg:pt-4">
+									<div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-lg">
+										<h2 class="text-base font-semibold leading-7 text-primary">Deploy faster</h2>
+										<p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</p>
+										<p class="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
+										<dl class="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+											<div v-for="feature in usage" :key="feature.name" class="relative pl-9">
+												<dt class="inline font-semibold text-gray-900">
+													<component :is="feature.icon" class="absolute left-1 top-1 h-5 w-5 text-accent" aria-hidden="true" />
+													{{ feature.name }}
+												</dt>
+												{{ ' ' }}
+												<dd class="inline">{{ feature.description }}</dd>
+											</div>
+										</dl>
+									</div>
+								</div>
+								<div class="sm:px-6 lg:px-0">
+									<div class="relative isolate overflow-hidden bg-primary-300 px-6 pt-8 sm:mx-auto sm:max-w-2xl sm:rounded-3xl sm:pl-16 sm:pr-0 sm:pt-16 lg:mx-0 lg:max-w-none">
+										<div class="absolute -inset-y-px -left-3 -z-10 w-full origin-bottom-left skew-x-[-30deg] bg-priamry-100 opacity-20 ring-1 ring-inset ring-white" aria-hidden="true" />
+										<div class="mx-auto max-w-2xl sm:mx-0 sm:max-w-none">
+											<img src="https://tailwindui.com/img/component-images/project-app-screenshot.png" alt="Product screenshot" width="2432" height="1442" class="-mb-12 w-[57rem] max-w-none rounded-tl-xl bg-gray-800 ring-1 ring-white/10" />
+										</div>
+										<div class="pointer-events-none absolute inset-0 ring-1 ring-inset ring-black/10 sm:rounded-3xl" aria-hidden="true" />
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
 
+					<!-- Benefits for students section -->
+					<section id="Benefits" class="relative isolate bg-white pb-32 pt-24 sm:pt-32">
+						<div class="absolute inset-x-0 top-1/2 -z-10 -translate-y-1/2 transform-gpu overflow-hidden opacity-30 blur-3xl" aria-hidden="true">
+							<div
+								class="ml-[max(50%,38rem)] aspect-[1313/771] w-[82.0625rem] bg-gradient-to-tr from-secondary to-primary"
+								style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+							/>
+						</div>
+						<div class="absolute inset-x-0 top-0 -z-10 flex transform-gpu overflow-hidden pt-32 opacity-25 blur-3xl sm:pt-40 xl:justify-end" aria-hidden="true">
+							<div
+								class="ml-[-22rem] aspect-[1313/771] w-[82.0625rem] flex-none origin-top-right rotate-[30deg] bg-gradient-to-tr from-accent to-darkaccent xl:ml-0 xl:mr-[calc(50%-12rem)]"
+								style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+							/>
+						</div>
+						<div class="mx-auto max-w-7xl px-6 lg:px-8">
+							<div class="mx-auto max-w-xl text-center">
+								<h2 class="text-lg font-semibold leading-8 tracking-tight text-primary">Testimonials</h2>
+								<p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">We have worked with thousands of amazing people</p>
+							</div>
+							<div class="mx-auto mt-16 grid max-w-2xl grid-cols-1 grid-rows-1 gap-8 text-sm leading-6 text-gray-900 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4">
+								<figure class="rounded-2xl bg-white shadow-lg ring-1 ring-gray-900/5 sm:col-span-2 xl:col-start-2 xl:row-end-1">
+									<blockquote class="p-6 text-lg font-semibold leading-7 tracking-tight text-gray-900 sm:p-12 sm:text-xl sm:leading-8">
+										<p>{{ `“${featuredTestimonial.body}”` }}</p>
+									</blockquote>
+									<figcaption class="flex flex-wrap items-center gap-x-4 gap-y-4 border-t border-gray-900/10 px-6 py-4 sm:flex-nowrap">
+										<img class="h-10 w-10 flex-none rounded-full bg-gray-50" :src="featuredTestimonial.author.imageUrl" alt="" />
+										<div class="flex-auto">
+											<div class="font-semibold">{{ featuredTestimonial.author.name }}</div>
+											<div class="text-gray-600">{{ `@${featuredTestimonial.author.handle}` }}</div>
+										</div>
+										<img class="h-10 w-auto flex-none" :src="featuredTestimonial.author.logoUrl" alt="" />
+									</figcaption>
+								</figure>
+								<div v-for="(columnGroup, columnGroupIdx) in testimonials" :key="columnGroupIdx" class="space-y-8 xl:contents xl:space-y-0">
+									<div v-for="(column, columnIdx) in columnGroup" :key="columnIdx" :class="[(columnGroupIdx === 0 && columnIdx === 0) || (columnGroupIdx === testimonials.length - 1 && columnIdx === columnGroup.length - 1) ? 'xl:row-span-2' : 'xl:row-start-1', 'space-y-8']">
+										<figure v-for="testimonial in column" :key="testimonial.author.handle" class="rounded-2xl bg-white p-6 shadow-lg ring-1 ring-gray-900/5">
+											<blockquote class="text-gray-900">
+												<p>{{ `“${testimonial.body}”` }}</p>
+											</blockquote>
+											<figcaption class="mt-6 flex items-center gap-x-4">
+												<img class="h-10 w-10 rounded-full bg-gray-50" :src="testimonial.author.imageUrl" alt="" />
+												<div>
+													<div class="font-semibold">{{ testimonial.author.name }}</div>
+													<div class="text-gray-600">{{ `@${testimonial.author.handle}` }}</div>
+												</div>
+											</figcaption>
+										</figure>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
 					<!-- Showcase section -->
-					<section id="Showcase" class="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-						<div class="bg-white pt-8 lg:pt-12">
+					<section id="Showcase">
+						<div class="pt-8 lg:pt-12">
 							<div class="mx-auto max-w-7xl px-6 lg:px-8">
 								<div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
 									<p class="text-base font-semibold leading-7 text-primary-300">Deploy faster</p>
@@ -86,7 +165,7 @@
 							</div>
 						</div>
 
-						<div class="bg-white py-24 sm:py-32">
+						<div class="py-24 sm:py-32">
 							<div class="mx-auto max-w-7xl px-6 lg:px-8">
 								<div class="mx-auto max-w-2xl lg:max-w-none">
 									<h2 class="text-lg font-semibold leading-8 text-gray-900">Trusted by the world’s most innovative teams</h2>
@@ -102,42 +181,39 @@
 						</div>
 					</section>
 					<!-- FAQ section -->
-					<section id="FAQ" class="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-						<div class="bg-white">
-							<div class="mx-auto max-w-7xl px-6 py-12 sm:py-16 lg:px-8 lg:py-20">
-								<div class="mx-auto max-w-4xl divide-y divide-gray-900/10">
-									<h2 class="text-2xl font-bold leading-10 tracking-tight text-gray-900">Frequently asked questions</h2>
-									<dl class="mt-10 space-y-6 divide-y divide-gray-900/10">
-										<Disclosure as="div" v-for="faq in faqs" :key="faq.question" class="pt-6" v-slot="{ open }">
-											<dt>
-												<DisclosureButton class="flex w-full items-start justify-between text-left text-gray-900">
-													<span class="text-base font-semibold leading-7">{{ faq.question }}</span>
-													<span class="ml-6 flex h-7 items-center">
-														<IconHeroicons:plus-20-solid v-if="!open" class="h-6 w-6" aria-hidden="true" />
-														<IconHeroicons:minus-20-solid v-else class="h-6 w-6" aria-hidden="true" />
-													</span>
-												</DisclosureButton>
-											</dt>
-											<DisclosurePanel as="dd" class="mt-2 pr-12">
-												<p class="text-base leading-7 text-gray-600">{{ faq.answer }}</p>
-											</DisclosurePanel>
-										</Disclosure>
-									</dl>
-								</div>
+					<section id="FAQ">
+						<div class="mx-auto max-w-7xl px-6 py-12 sm:py-16 lg:px-8 lg:py-20">
+							<div class="mx-auto max-w-4xl divide-y divide-gray-900/10">
+								<h2 class="text-2xl font-bold leading-10 tracking-tight text-gray-900">Frequently asked questions</h2>
+								<dl class="mt-10 space-y-6 divide-y divide-gray-900/10">
+									<Disclosure as="div" v-for="faq in faqs" :key="faq.question" class="pt-6" v-slot="{ open }">
+										<dt>
+											<DisclosureButton class="flex w-full items-start justify-between text-left text-gray-900">
+												<span class="text-base font-semibold leading-7">{{ faq.question }}</span>
+												<span class="ml-6 flex h-7 items-center">
+													<IconHeroicons:plus-20-solid v-if="!open" class="h-6 w-6" aria-hidden="true" />
+													<IconHeroicons:minus-20-solid v-else class="h-6 w-6" aria-hidden="true" />
+												</span>
+											</DisclosureButton>
+										</dt>
+										<DisclosurePanel as="dd" class="mt-2 pr-12">
+											<p class="text-base leading-7 text-gray-600">{{ faq.answer }}</p>
+										</DisclosurePanel>
+									</Disclosure>
+								</dl>
 							</div>
 						</div>
 					</section>
+
 					<!-- CTA section -->
-					<section id="CTA" class="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
-						<div class="bg-white">
-							<div class="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
-								<div class="mx-auto max-w-2xl text-center">
-									<h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Boost your productivity.<br />Start using our app today.</h2>
-									<p class="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-600">Incididunt sint fugiat pariatur cupidatat consectetur sit cillum anim id veniam aliqua proident excepteur commodo do ea.</p>
-									<div class="mt-10 flex items-center justify-center gap-x-6">
-										<a href="#" class="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">Get started</a>
-										<a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
-									</div>
+					<section id="CTA">
+						<div class="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
+							<div class="mx-auto max-w-2xl text-center">
+								<h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Boost your productivity.<br />Start using our app today.</h2>
+								<p class="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-600">Incididunt sint fugiat pariatur cupidatat consectetur sit cillum anim id veniam aliqua proident excepteur commodo do ea.</p>
+								<div class="mt-10 flex items-center justify-center gap-x-6">
+									<a href="#" class="rounded-md bg-primary px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">Get started</a>
+									<a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a>
 								</div>
 							</div>
 						</div>
@@ -157,6 +233,26 @@
 <script setup>
 import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue';
 
+import CloudArrowUpIcon from '~icons/fluent/grid-24-filled';
+import LockClosedIcon from '~icons/fluent/grid-24-filled';
+import ServerIcon from '~icons/fluent/grid-24-filled';
+const usage = [
+	{
+		name: 'Push to deploy.',
+		description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+		icon: CloudArrowUpIcon
+	},
+	{
+		name: 'SSL certificates.',
+		description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+		icon: LockClosedIcon
+	},
+	{
+		name: 'Database backups.',
+		description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+		icon: ServerIcon
+	}
+];
 const features = [
 	{
 		name: 'Invite team members',
@@ -181,6 +277,65 @@ const features = [
 		name: 'Mobile app',
 		description: 'Nulla est saepe accusamus nostrum est est. Fugit voluptatum omnis quidem voluptatem.'
 	}
+];
+const featuredTestimonial = {
+	body: 'Integer id nunc sit semper purus. Bibendum at lacus ut arcu blandit montes vitae auctor libero. Hac condimentum dignissim nibh vulputate ut nunc. Amet nibh orci mi venenatis blandit vel et proin. Non hendrerit in vel ac diam.',
+	author: {
+		name: 'Brenna Goyette',
+		handle: 'brennagoyette',
+		imageUrl: 'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=1024&h=1024&q=80',
+		logoUrl: 'https://tailwindui.com/img/logos/savvycal-logo-gray-900.svg'
+	}
+};
+const testimonials = [
+	[
+		[
+			{
+				body: 'Laborum quis quam. Dolorum et ut quod quia. Voluptas numquam delectus nihil. Aut enim doloremque et ipsam.',
+				author: {
+					name: 'Leslie Alexander',
+					handle: 'lesliealexander',
+					imageUrl: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+				}
+			}
+			// More testimonials...
+		],
+		[
+			{
+				body: 'Aut reprehenderit voluptatem eum asperiores beatae id. Iure molestiae ipsam ut officia rem nulla blanditiis.',
+				author: {
+					name: 'Lindsay Walton',
+					handle: 'lindsaywalton',
+					imageUrl: 'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+				}
+			}
+			// More testimonials...
+		]
+	],
+	[
+		[
+			{
+				body: 'Voluptas quos itaque ipsam in voluptatem est. Iste eos blanditiis repudiandae. Earum deserunt enim molestiae ipsum perferendis recusandae saepe corrupti.',
+				author: {
+					name: 'Tom Cook',
+					handle: 'tomcook',
+					imageUrl: 'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+				}
+			}
+			// More testimonials...
+		],
+		[
+			{
+				body: 'Molestias ea earum quos nostrum doloremque sed. Quaerat quasi aut velit incidunt excepturi rerum voluptatem minus harum.',
+				author: {
+					name: 'Leonard Krasner',
+					handle: 'leonardkrasner',
+					imageUrl: 'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+				}
+			}
+			// More testimonials...
+		]
+	]
 ];
 const stats = [
 	{ label: 'Founded', value: '2021' },
