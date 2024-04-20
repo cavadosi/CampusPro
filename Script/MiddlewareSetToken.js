679e3945a9ddb454d095bbd88add471eb9ed97e2
Marc Pastor <marcpastorllopart@gmail.com>
2024-04-20 16:43:10 +0200

    new urls

---
+++ Script/MiddlewareSetToken.js
@@ -0,0 +1,16 @@
+export const setAuthorizationToken = () => {
+  return localStorage.getItem('token');
+};
+
+axios.interceptors.request.use(
+  config => {
+    const token = getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
