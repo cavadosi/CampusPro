02e7e5ab104c8e3eb786bb82732a012910624745
cavadosi <jpeque.moab@gmail.com>
2024-04-22 10:46:23 +0200

    FIX imports #2

--- assets/script/getBaseUrl.js
+++ assets/script/getBaseUrl.js
@@ -2,11 +2,11 @@
 
 // dotenv.config();
 
-// export const getBaseUrl = () => {
-// 	if (process.env.DOMAIN === 'development') {
-// 		console.log('development');
-// 		return 'http://localhost:3000';
-// 	} else {
-// 		return 'https://campus-pro.vercel.app';
-// 	}
-// };
+export const getBaseUrl = () => {
+	if (process.env.DOMAIN === 'development') {
+		console.log('development');
+		return 'http://localhost:3000';
+	} else {
+		return 'https://campus-pro.vercel.app';
+	}
+};
