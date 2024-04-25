50e3f573a58783c30bc2399ce39ac6f6097b069b
unknown <andresesanabria9@gmail.com>
2024-04-25 12:17:06 +0200

    About page dark mode

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,59 +1,10 @@
 <template>
 	<div class="bg-bgcover transition-colors duration-300">
-		<!-- Header -->
-		<header class="absolute inset-x-0 top-0 z-50">
-			<nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-				<div class="flex lg:flex-1">
-					<a href="#" class="-m-1.5 p-1.5">
-						<span class="sr-only">Your Company</span>
-						<img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="" />
-					</a>
-				</div>
-				<div class="flex lg:hidden">
-					<button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400" @click="mobileMenuOpen = true">
-						<span class="sr-only">Open main menu</span>
-						<Bars3Icon class="h-6 w-6" aria-hidden="true" />
-					</button>
-				</div>
-				<div class="hidden lg:flex lg:gap-x-12">
-					<a v-for="item in navigation" :key="item.name" :href="item.href" class="text-sm font-semibold leading-6 text-white">{{ item.name }}</a>
-				</div>
-				<div class="hidden lg:flex lg:flex-1 lg:justify-end">
-					<a href="#" class="text-sm font-semibold leading-6 text-white">Log in <span aria-hidden="true">&rarr;</span></a>
-				</div>
-			</nav>
-			<Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
-				<div class="fixed inset-0 z-50" />
-				<DialogPanel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
-					<div class="flex items-center justify-between">
-						<a href="#" class="-m-1.5 p-1.5">
-							<span class="sr-only">Your Company</span>
-							<img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="" />
-						</a>
-						<button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-400" @click="mobileMenuOpen = false">
-							<span class="sr-only">Close menu</span>
-							<XMarkIcon class="h-6 w-6" aria-hidden="true" />
-						</button>
-					</div>
-					<div class="mt-6 flow-root">
-						<div class="-my-6 divide-y divide-gray-500/25">
-							<div class="space-y-2 py-6">
-								<a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800">{{ item.name }}</a>
-							</div>
-							<div class="py-6">
-								<a href="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-800">Log in</a>
-							</div>
-						</div>
-					</div>
-				</DialogPanel>
-			</Dialog>
-		</header>
-
 		<main class="relative isolate">
 			<!-- Background -->
 			<div class="absolute inset-x-0 top-4 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl" aria-hidden="true">
 				<div
-					class="aspect-[1108/632] w-[69.25rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-25"
+					class="aspect-[1108/632] w-[69.25rem] flex-none bg-gradient-to-r from-primary to-accent opacity-25"
 					style="clip-path: polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)"
 				/>
 			</div>
@@ -61,15 +12,15 @@
 			<!-- Header section -->
 			<div class="px-6 pt-14 lg:px-8">
 				<div class="mx-auto max-w-2xl pt-24 text-center sm:pt-40">
-					<h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">We love creators</h2>
-					<p class="mt-6 text-lg leading-8 text-gray-300">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+					<h2 class="text-4xl font-bold tracking-tight text-tbase sm:text-6xl">We love creators</h2>
+					<p class="mt-6 text-lg leading-8 text-tlight">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
 				</div>
 			</div>
 
 			<!-- Content section -->
 			<div class="mx-auto mt-20 max-w-7xl px-6 lg:px-8">
 				<div class="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
-					<div class="grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-300 lg:max-w-none lg:grid-cols-2">
+					<div class="grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-tlight lg:max-w-none lg:grid-cols-2">
 						<div>
 							<p>
 								Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat.
@@ -86,9 +37,9 @@
 						</div>
 					</div>
 					<dl class="mt-16 grid grid-cols-1 gap-x-8 gap-y-12 sm:mt-20 sm:grid-cols-2 sm:gap-y-16 lg:mt-28 lg:grid-cols-4">
-						<div v-for="(stat, statIdx) in stats" :key="statIdx" class="flex flex-col-reverse gap-y-3 border-l border-white/20 pl-6">
-							<dt class="text-base leading-7 text-gray-300">{{ stat.label }}</dt>
-							<dd class="text-3xl font-semibold tracking-tight text-white">{{ stat.value }}</dd>
+						<div v-for="(stat, statIdx) in stats" :key="statIdx" class="flex flex-col-reverse gap-y-3 border-l border-tbase/20 pl-6">
+							<dt class="text-base leading-7 text-tlight">{{ stat.label }}</dt>
+							<dd class="text-3xl font-semibold tracking-tight text-tbase">{{ stat.value }}</dd>
 						</div>
 					</dl>
 				</div>
@@ -102,12 +53,12 @@
 			<!-- Values section -->
 			<div class="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8">
 				<div class="mx-auto max-w-2xl lg:mx-0">
-					<h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Our values</h2>
-					<p class="mt-6 text-lg leading-8 text-gray-300">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis.</p>
+					<h2 class="text-3xl font-bold tracking-tight text-tbase sm:text-4xl">Our values</h2>
+					<p class="mt-6 text-lg leading-8 text-tlight">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis.</p>
 				</div>
-				<dl class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 text-base leading-7 text-gray-300 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:gap-x-16">
+				<dl class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 text-base leading-7 text-tlight sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:gap-x-16">
 					<div v-for="value in values" :key="value.name" class="relative pl-9">
-						<dt class="inline font-semibold text-white">
+						<dt class="inline font-semibold text-tbase">
 							<component :is="value.icon" class="absolute left-1 top-1 h-5 w-5 text-indigo-500" aria-hidden="true" />
 							{{ value.name }}
 						</dt>
@@ -120,119 +71,24 @@
 			<!-- Team section -->
 			<div class="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8">
 				<div class="mx-auto max-w-2xl lg:mx-0">
-					<h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Our team</h2>
-					<p class="mt-6 text-lg leading-8 text-gray-300">Excepturi repudiandae alias ut. Totam aut facilis. Praesentium in neque vel omnis. Eos error odio. Qui fugit voluptatibus eum culpa.</p>
+					<h2 class="text-3xl font-bold tracking-tight text-tbase sm:text-4xl">Our team</h2>
+					<p class="mt-6 text-lg leading-8 text-tlight">Excepturi repudiandae alias ut. Totam aut facilis. Praesentium in neque vel omnis. Eos error odio. Qui fugit voluptatibus eum culpa.</p>
 				</div>
 				<ul role="list" class="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-14 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4">
 					<li v-for="person in team" :key="person.name">
 						<img class="aspect-[14/13] w-full rounded-2xl object-cover" :src="person.imageUrl" alt="" />
-						<h3 class="mt-6 text-lg font-semibold leading-8 tracking-tight text-white">{{ person.name }}</h3>
-						<p class="text-base leading-7 text-gray-300">{{ person.role }}</p>
-						<p class="text-sm leading-6 text-gray-500">{{ person.location }}</p>
+						<h3 class="mt-6 text-lg font-semibold leading-8 tracking-tight text-tbase">{{ person.name }}</h3>
+						<p class="text-base leading-7 text-tlight">{{ person.role }}</p>
+						<p class="text-sm leading-6 text-tlight">{{ person.location }}</p>
 					</li>
 				</ul>
 			</div>
-
-			<!-- CTA section -->
-			<div class="relative isolate -z-10 mt-32 sm:mt-40">
-				<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-					<div class="mx-auto flex max-w-2xl flex-col gap-16 bg-white/5 px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
-						<img
-							class="h-96 w-full flex-none rounded-2xl object-cover shadow-xl lg:aspect-square lg:h-auto lg:max-w-sm"
-							src="https://images.unsplash.com/photo-1519338381761-c7523edc1f46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-							alt=""
-						/>
-						<div class="w-full flex-auto">
-							<h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Join our team</h2>
-							<p class="mt-6 text-lg leading-8 text-gray-300">Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.</p>
-							<ul role="list" class="mt-10 grid grid-cols-1 gap-x-8 gap-y-3 text-base leading-7 text-white sm:grid-cols-2">
-								<li v-for="benefit in benefits" :key="benefit" class="flex gap-x-3">
-									<CheckCircleIcon class="h-7 w-5 flex-none" aria-hidden="true" />
-									{{ benefit }}
-								</li>
-							</ul>
-							<div class="mt-10 flex">
-								<a href="#" class="text-sm font-semibold leading-6 text-indigo-400">See our job postings <span aria-hidden="true">&rarr;</span></a>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="absolute inset-x-0 -top-16 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl" aria-hidden="true">
-					<div
-						class="aspect-[1318/752] w-[82.375rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-25"
-						style="clip-path: polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)"
-					/>
-				</div>
-			</div>
 		</main>
-
-		<!-- Footer -->
-		<footer class="relative mt-32 sm:mt-40" aria-labelledby="footer-heading">
-			<h2 id="footer-heading" class="sr-only">Footer</h2>
-			<div class="mx-auto max-w-7xl px-6 pb-8 lg:px-8">
-				<div class="xl:grid xl:grid-cols-3 xl:gap-8">
-					<div class="space-y-8">
-						<img class="h-7" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Company name" />
-						<p class="text-sm leading-6 text-gray-300">Est error fuga modi error. Laborum eum nobis porro cupiditate et quo.</p>
-						<div class="flex space-x-6">
-							<a v-for="item in footerNavigation.social" :key="item.name" :href="item.href" class="text-gray-500 hover:text-gray-400">
-								<span class="sr-only">{{ item.name }}</span>
-								<component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
-							</a>
-						</div>
-					</div>
-					<div class="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
-						<div class="md:grid md:grid-cols-2 md:gap-8">
-							<div>
-								<h3 class="text-sm font-semibold leading-6 text-white">Solutions</h3>
-								<ul role="list" class="mt-6 space-y-4">
-									<li v-for="item in footerNavigation.solutions" :key="item.name">
-										<a :href="item.href" class="text-sm leading-6 text-gray-300 hover:text-white">{{ item.name }}</a>
-									</li>
-								</ul>
-							</div>
-							<div class="mt-10 md:mt-0">
-								<h3 class="text-sm font-semibold leading-6 text-white">Support</h3>
-								<ul role="list" class="mt-6 space-y-4">
-									<li v-for="item in footerNavigation.support" :key="item.name">
-										<a :href="item.href" class="text-sm leading-6 text-gray-300 hover:text-white">{{ item.name }}</a>
-									</li>
-								</ul>
-							</div>
-						</div>
-						<div class="md:grid md:grid-cols-2 md:gap-8">
-							<div>
-								<h3 class="text-sm font-semibold leading-6 text-white">Company</h3>
-								<ul role="list" class="mt-6 space-y-4">
-									<li v-for="item in footerNavigation.company" :key="item.name">
-										<a :href="item.href" class="text-sm leading-6 text-gray-300 hover:text-white">{{ item.name }}</a>
-									</li>
-								</ul>
-							</div>
-							<div class="mt-10 md:mt-0">
-								<h3 class="text-sm font-semibold leading-6 text-white">Legal</h3>
-								<ul role="list" class="mt-6 space-y-4">
-									<li v-for="item in footerNavigation.legal" :key="item.name">
-										<a :href="item.href" class="text-sm leading-6 text-gray-300 hover:text-white">{{ item.name }}</a>
-									</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="mt-16 border-t border-white/10 pt-8 sm:mt-20 lg:mt-24">
-					<p class="text-xs leading-5 text-gray-400">&copy; 2020 Your Company, Inc. All rights reserved.</p>
-				</div>
-			</div>
-		</footer>
 	</div>
 </template>
 
 <script setup>
 import { defineComponent, h, ref } from 'vue';
-import { Dialog, DialogPanel } from '@headlessui/vue';
-import { Bars3Icon, XMarkIcon } from '@heroicons/vue/24/outline';
-import { AcademicCapIcon, CheckCircleIcon, HandRaisedIcon, RocketLaunchIcon, SparklesIcon, SunIcon, UserGroupIcon } from '@heroicons/vue/20/solid';
 
 const navigation = [
 	{ name: 'Product', href: '#' },
@@ -249,33 +105,33 @@ const stats = [
 const values = [
 	{
 		name: 'Be world-class.',
-		description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.',
-		icon: RocketLaunchIcon
+		description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit aute id magna.'
+		// icon: RocketLaunchIcon
 	},
 	{
 		name: 'Take responsibility.',
-		description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-		icon: HandRaisedIcon
+		description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.'
+		// icon: HandRaisedIcon
 	},
 	{
 		name: 'Be supportive.',
-		description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus voluptas blanditiis et.',
-		icon: UserGroupIcon
+		description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus voluptas blanditiis et.'
+		// icon: UserGroupIcon
 	},
 	{
 		name: 'Always learning.',
-		description: 'Iure sed ab. Aperiam optio placeat dolor facere. Officiis pariatur eveniet atque et dolor.',
-		icon: AcademicCapIcon
+		description: 'Iure sed ab. Aperiam optio placeat dolor facere. Officiis pariatur eveniet atque et dolor.'
+		// icon: AcademicCapIcon
 	},
 	{
 		name: 'Share everything you know.',
-		description: 'Laudantium tempora sint ut consectetur ratione. Ut illum ut rem numquam fuga delectus.',
-		icon: SparklesIcon
+		description: 'Laudantium tempora sint ut consectetur ratione. Ut illum ut rem numquam fuga delectus.'
+		// icon: SparklesIcon
 	},
 	{
 		name: 'Enjoy downtime.',
-		description: 'Culpa dolorem voluptatem velit autem rerum qui et corrupti. Quibusdam quo placeat.',
-		icon: SunIcon
+		description: 'Culpa dolorem voluptatem velit autem rerum qui et corrupti. Quibusdam quo placeat.'
+		// icon: SunIcon
 	}
 ];
 const team = [
