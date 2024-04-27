9925e919bf130a9721c936a52397d738d3b8bd59
cavadosi <jpeque.moab@gmail.com>
2024-04-27 16:12:25 +0200

    Steps and activeStep

--- components/RegisterForm.vue
+++ components/RegisterForm.vue
@@ -1,65 +1,206 @@
 <template>
-	<div>
-		<div class="px-4 sm:px-0">
-			<h3 class="text-base font-semibold leading-7 text-gray-900">Applicant Information</h3>
-			<p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Personal details and application.</p>
-		</div>
-		<div class="mt-6 border-t border-gray-100">
-			<dl class="divide-y divide-gray-100">
-				<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-					<dt class="text-sm font-medium leading-6 text-gray-900">Full name</dt>
-					<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot Foster</dd>
-				</div>
-				<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-					<dt class="text-sm font-medium leading-6 text-gray-900">Application for</dt>
-					<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Backend Developer</dd>
-				</div>
-				<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-					<dt class="text-sm font-medium leading-6 text-gray-900">Email address</dt>
-					<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">margotfoster@example.com</dd>
-				</div>
-				<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-					<dt class="text-sm font-medium leading-6 text-gray-900">Salary expectation</dt>
-					<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">$120,000</dd>
-				</div>
-				<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-					<dt class="text-sm font-medium leading-6 text-gray-900">About</dt>
-					<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-						Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea officia proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
-					</dd>
-				</div>
-				<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-					<dt class="text-sm font-medium leading-6 text-gray-900">Attachments</dt>
-					<dd class="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-						<ul role="list" class="divide-y divide-gray-100 rounded-md border border-gray-200">
-							<li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-								<div class="flex w-0 flex-1 items-center">
-									<div class="ml-4 flex min-w-0 flex-1 gap-2">
-										<span class="truncate font-medium">resume_back_end_developer.pdf</span>
-										<span class="flex-shrink-0 text-gray-400">2.4mb</span>
+	<div class="block items-center justify-start">
+		<section v-if="activeStep.id === 0">
+			<div class="px-4 sm:px-0">
+				<h3 class="text-base font-semibold leading-7 text-tbase">{{ activeStep.title }}</h3>
+				<p class="mt-1 max-w-2xl text-sm leading-6 text-tlight">{{ activeStep.description }}</p>
+			</div>
+			<div class="mt-6 border-t border-tbase">
+				<dl class="divide-y divide-tlight">
+					<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+						<dt class="text-sm font-medium leading-6 text-tbase">Full name</dt>
+						<dd class="mt-1 text-sm leading-6 text-tlight sm:col-span-2 sm:mt-0">Margot Foster</dd>
+					</div>
+
+					<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+						<dt class="text-sm font-medium leading-6 text-gray-900">Attachments</dt>
+						<dd class="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+							<ul role="list" class="divide-y divide-gray-100 rounded-md border border-gray-200">
+								<li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+									<div class="flex w-0 flex-1 items-center">
+										<div class="ml-4 flex min-w-0 flex-1 gap-2">
+											<span class="truncate font-medium">resume_back_end_developer.pdf</span>
+											<span class="flex-shrink-0 text-gray-400">2.4mb</span>
+										</div>
 									</div>
-								</div>
-								<div class="ml-4 flex-shrink-0">
-									<a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
-								</div>
-							</li>
-							<li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-								<div class="flex w-0 flex-1 items-center">
-									<div class="ml-4 flex min-w-0 flex-1 gap-2">
-										<span class="truncate font-medium">coverletter_back_end_developer.pdf</span>
-										<span class="flex-shrink-0 text-gray-400">4.5mb</span>
+									<div class="ml-4 flex-shrink-0">
+										<a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
 									</div>
-								</div>
-								<div class="ml-4 flex-shrink-0">
-									<a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
-								</div>
-							</li>
-						</ul>
-					</dd>
-				</div>
-			</dl>
-		</div>
+								</li>
+								<li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+									<div class="flex w-0 flex-1 items-center">
+										<div class="ml-4 flex min-w-0 flex-1 gap-2">
+											<span class="truncate font-medium">coverletter_back_end_developer.pdf</span>
+											<span class="flex-shrink-0 text-gray-400">4.5mb</span>
+										</div>
+									</div>
+									<div class="ml-4 flex-shrink-0">
+										<a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
+									</div>
+								</li>
+							</ul>
+						</dd>
+					</div>
+				</dl>
+			</div>
+		</section>
+		<section v-if="activeStep.id === 1">
+			<div class="px-4 sm:px-0">
+				<h3 class="text-base font-semibold leading-7 text-tbase">{{ activeStep.title }}</h3>
+				<p class="mt-1 max-w-2xl text-sm leading-6 text-tlight">{{ activeStep.description }}</p>
+			</div>
+			<div class="mt-6 border-t border-tbase">
+				<dl class="divide-y divide-tlight">
+					<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+						<dt class="text-sm font-medium leading-6 text-tbase">Full name</dt>
+						<dd class="mt-1 text-sm leading-6 text-tlight sm:col-span-2 sm:mt-0">Margot Foster</dd>
+					</div>
+
+					<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+						<dt class="text-sm font-medium leading-6 text-gray-900">Attachments</dt>
+						<dd class="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+							<ul role="list" class="divide-y divide-gray-100 rounded-md border border-gray-200">
+								<li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+									<div class="flex w-0 flex-1 items-center">
+										<div class="ml-4 flex min-w-0 flex-1 gap-2">
+											<span class="truncate font-medium">resume_back_end_developer.pdf</span>
+											<span class="flex-shrink-0 text-gray-400">2.4mb</span>
+										</div>
+									</div>
+									<div class="ml-4 flex-shrink-0">
+										<a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
+									</div>
+								</li>
+								<li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+									<div class="flex w-0 flex-1 items-center">
+										<div class="ml-4 flex min-w-0 flex-1 gap-2">
+											<span class="truncate font-medium">coverletter_back_end_developer.pdf</span>
+											<span class="flex-shrink-0 text-gray-400">4.5mb</span>
+										</div>
+									</div>
+									<div class="ml-4 flex-shrink-0">
+										<a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
+									</div>
+								</li>
+							</ul>
+						</dd>
+					</div>
+				</dl>
+			</div>
+		</section>
+		<section v-if="activeStep.id === 2">
+			<div class="px-4 sm:px-0">
+				<h3 class="text-base font-semibold leading-7 text-tbase">{{ activeStep.title }}</h3>
+				<p class="mt-1 max-w-2xl text-sm leading-6 text-tlight">{{ activeStep.description }}</p>
+			</div>
+			<div class="mt-6 border-t border-tbase">
+				<dl class="divide-y divide-tlight">
+					<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+						<dt class="text-sm font-medium leading-6 text-tbase">Full name</dt>
+						<dd class="mt-1 text-sm leading-6 text-tlight sm:col-span-2 sm:mt-0">Margot Foster</dd>
+					</div>
+
+					<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+						<dt class="text-sm font-medium leading-6 text-gray-900">Attachments</dt>
+						<dd class="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+							<ul role="list" class="divide-y divide-gray-100 rounded-md border border-gray-200">
+								<li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+									<div class="flex w-0 flex-1 items-center">
+										<div class="ml-4 flex min-w-0 flex-1 gap-2">
+											<span class="truncate font-medium">resume_back_end_developer.pdf</span>
+											<span class="flex-shrink-0 text-gray-400">2.4mb</span>
+										</div>
+									</div>
+									<div class="ml-4 flex-shrink-0">
+										<a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
+									</div>
+								</li>
+								<li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+									<div class="flex w-0 flex-1 items-center">
+										<div class="ml-4 flex min-w-0 flex-1 gap-2">
+											<span class="truncate font-medium">coverletter_back_end_developer.pdf</span>
+											<span class="flex-shrink-0 text-gray-400">4.5mb</span>
+										</div>
+									</div>
+									<div class="ml-4 flex-shrink-0">
+										<a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
+									</div>
+								</li>
+							</ul>
+						</dd>
+					</div>
+				</dl>
+			</div>
+		</section>
+		<section v-if="activeStep.id === 3">
+			<div class="px-4 sm:px-0">
+				<h3 class="text-base font-semibold leading-7 text-tbase">{{ activeStep.title }}</h3>
+				<p class="mt-1 max-w-2xl text-sm leading-6 text-tlight">{{ activeStep.description }}</p>
+			</div>
+			<div class="mt-6 border-t border-tbase">
+				<dl class="divide-y divide-tlight">
+					<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+						<dt class="text-sm font-medium leading-6 text-tbase">Full name</dt>
+						<dd class="mt-1 text-sm leading-6 text-tlight sm:col-span-2 sm:mt-0">Margot Foster</dd>
+					</div>
+
+					<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+						<dt class="text-sm font-medium leading-6 text-gray-900">Attachments</dt>
+						<dd class="mt-2 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+							<ul role="list" class="divide-y divide-gray-100 rounded-md border border-gray-200">
+								<li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+									<div class="flex w-0 flex-1 items-center">
+										<div class="ml-4 flex min-w-0 flex-1 gap-2">
+											<span class="truncate font-medium">resume_back_end_developer.pdf</span>
+											<span class="flex-shrink-0 text-gray-400">2.4mb</span>
+										</div>
+									</div>
+									<div class="ml-4 flex-shrink-0">
+										<a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
+									</div>
+								</li>
+								<li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+									<div class="flex w-0 flex-1 items-center">
+										<div class="ml-4 flex min-w-0 flex-1 gap-2">
+											<span class="truncate font-medium">coverletter_back_end_developer.pdf</span>
+											<span class="flex-shrink-0 text-gray-400">4.5mb</span>
+										</div>
+									</div>
+									<div class="ml-4 flex-shrink-0">
+										<a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Download</a>
+									</div>
+								</li>
+							</ul>
+						</dd>
+					</div>
+				</dl>
+			</div>
+		</section>
 	</div>
 </template>
 
-<script setup></script>
+<script setup>
+const steps = [
+	{
+		id: 0,
+		title: 'Contact info',
+		description: 'add your institution contact info'
+	},
+	{
+		id: 1,
+		title: 'Contact info1',
+		description: 'add your institution contact info'
+	},
+	{
+		id: 2,
+		title: 'Contact info2',
+		description: 'add your institution contact info'
+	},
+	{
+		id: 3,
+		title: 'Contact info3',
+		description: 'add your institution contact info'
+	}
+];
+const activeStep = ref(steps[3]);
+</script>
