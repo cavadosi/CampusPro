ca13eec58aaf00a1f1996b4fd76afe9a7ab4bc96
cavadosi <jpeque.moab@gmail.com>
2024-05-18 18:16:03 +0200

    login page performance enhancement

--- pages/login.vue
+++ pages/login.vue
@@ -72,7 +72,7 @@
 			</div>
 		</div>
 		<div class="relative hidden w-0 flex-1 lg:block">
-			<img class="absolute inset-0 h-full w-full object-cover" src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80" alt="" />
+			<NuxtImg loading="lazy" format="webp" class="absolute inset-0 h-full w-full object-cover" src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80" alt="" />
 		</div>
 	</div>
 </template>
